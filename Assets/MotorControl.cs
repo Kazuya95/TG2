8fd7a9b9cb8934bab136bda111ad9cbe288b7af6
Kazuya95 <32460031+Kazuya95@users.noreply.github.com>
2022-02-13 16:05:47 -0300

    Add files via upload

--- Assets/MotorControl.cs
+++ Assets/MotorControl.cs
@@ -13,15 +13,32 @@ public class MotorControl : MonoBehaviour{
 	public GameObject Switch;
 	public float velocity;
 	
-	
+	//to read data
 	void Update(){
-
+		// repeat the read of velocity data
+		StartCoroutine(ReadAxisVelocity());
+	}
+	
+	// Coroutine to wait 15s to show velocity of axis
+	IEnumerator ReadAxisVelocity(){
 		var hinge = Axis.GetComponent<HingeJoint>();
+        //Print the time of when the function is first called.
 		// variable to read current velocity in degrees/s
 		string statusVelocity = string.Format("current degrees/s: {0}.",hinge.velocity);
 		Debug.Log(statusVelocity);
-	}
+
+        //yield on a new YieldInstruction that waits for 5 seconds.
+        yield return new WaitForSeconds(15);
+    }
 	
+	// Control the activation of motor
+	void MotorSystem(bool enableMotor,bool stateMotor,bool fixedAxis){
+		var hinge = Axis.GetComponent<HingeJoint>();
+		hinge.useMotor = enableMotor;
+		motorIsOn = stateMotor;
+		var AxisRigidbody = Axis.GetComponent<Rigidbody>();
+		AxisRigidbody.freezeRotation = fixedAxis;
+	}
 	
     void OnMouseDown(){ //THIS FUNCTION WILL DETECT THE MOUSE CLICK ON A COLLIDER,IN OUR CASE WILL DETECT THE CLICK ON THE BUTTON
 		// Get the renderer from ButtonMotor
@@ -30,29 +47,24 @@ public class MotorControl : MonoBehaviour{
 		var motor = hinge.motor;
 		var AxisRigidbody = Axis.GetComponent<Rigidbody>();
 		AxisRigidbody.freezeRotation = true;
+		
 		var switchAction = Switch.GetComponent<Switch_action>();
 		var switchState = switchAction.switchIsOn;
 		
 		if (motorIsOn == false && switchState==true){
-			motorIsOn = true;
+			MotorSystem(true,true,false);
 			motorButtonRenderer.material.SetColor("_Color",colorOn);
-			AxisRigidbody.freezeRotation = false;
 			motor.force = 100*10000;
 			// targetVelocity make the hinge motor rotate with degrees per second
 			// adapting this for rad/s
 			motor.targetVelocity = 3600;
 			motor.freeSpin = false;
 			hinge.motor = motor;
-			hinge.useMotor = true;
 		}
 		else{
-			hinge.useMotor = false;
-			motorIsOn = false;
-			AxisRigidbody.freezeRotation = true;
+			MotorSystem(false,false,true);
 			motorButtonRenderer.material.SetColor("_Color",colorOff);
 		}
-		
-           
        
     }
 
--- Assets/Switch_action.cs
+++ Assets/Switch_action.cs
@@ -8,11 +8,21 @@ public class Switch_action : MonoBehaviour{
     public bool switchIsOn;
 	Color colorOn = Color.green;
 	Color colorOff = Color.red;
+	public GameObject Axis;
+	public GameObject ButtonMotor;
 
 	
     void OnMouseDown(){ //THIS FUNCTION WILL DETECT THE MOUSE CLICK ON A COLLIDER,IN OUR CASE WILL DETECT THE CLICK ON THE BUTTON
 		// Get the renderer from Switch
 		var switchRenderer = Switch.GetComponent<Renderer>(); 
+		var hinge = Axis.GetComponent<HingeJoint>();
+		var motor = hinge.motor;
+		var AxisRigidbody = Axis.GetComponent<Rigidbody>();
+		AxisRigidbody.freezeRotation = true;
+		var motorButtonRenderer = ButtonMotor.GetComponent<Renderer>(); 
+		
+		var colorButtonMotor = Switch.GetComponent<Switch_action>();
+		
 		if (switchIsOn == false){
 			switchIsOn = true;
 			switchRenderer.material.SetColor("_Color",colorOn);
@@ -20,6 +30,9 @@ public class Switch_action : MonoBehaviour{
 		else{
 			switchIsOn = false;
 			switchRenderer.material.SetColor("_Color",colorOff);
+			hinge.useMotor = false;
+			AxisRigidbody.freezeRotation = true;
+			motorButtonRenderer.material.SetColor("_Color",colorOff);
 		}
        
     }
