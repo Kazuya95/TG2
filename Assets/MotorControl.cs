f0e393fd22a9d3afdcf2502191b96081f527625f
Kazuya95 <32460031+Kazuya95@users.noreply.github.com>
2022-02-13 12:08:38 -0300

    Add files via upload

--- Assets/MotorControl.cs
+++ Assets/MotorControl.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class MotorControl : MonoBehaviour{
 
     public GameObject ButtonMotor;
@@ -9,40 +10,49 @@ public class MotorControl : MonoBehaviour{
 	Color colorOn = Color.green;
 	Color colorOff = Color.red;
 	public GameObject Axis;
+	public GameObject Switch;
+	public float velocity;
+	
+	
+	void Update(){
+
+		var hinge = Axis.GetComponent<HingeJoint>();
+		// variable to read current velocity in degrees/s
+		string statusVelocity = string.Format("current degrees/s: {0}.",hinge.velocity);
+		Debug.Log(statusVelocity);
+	}
 	
 	
     void OnMouseDown(){ //THIS FUNCTION WILL DETECT THE MOUSE CLICK ON A COLLIDER,IN OUR CASE WILL DETECT THE CLICK ON THE BUTTON
 		// Get the renderer from ButtonMotor
-		var motorRenderer = ButtonMotor.GetComponent<Renderer>(); 
+		var motorButtonRenderer = ButtonMotor.GetComponent<Renderer>(); 
 		var hinge = Axis.GetComponent<HingeJoint>();
 		var motor = hinge.motor;
 		var AxisRigidbody = Axis.GetComponent<Rigidbody>();
 		AxisRigidbody.freezeRotation = true;
+		var switchAction = Switch.GetComponent<Switch_action>();
+		var switchState = switchAction.switchIsOn;
 		
-		if (motorIsOn == false){
+		if (motorIsOn == false && switchState==true){
 			motorIsOn = true;
-			motorRenderer.material.SetColor("_Color",colorOn);
+			motorButtonRenderer.material.SetColor("_Color",colorOn);
 			AxisRigidbody.freezeRotation = false;
-			motor.force = 100;
-			// Make the hinge motor rotate with 90 degrees per second and a strong force.
-			motor.targetVelocity = 90;
+			motor.force = 100*10000;
+			// targetVelocity make the hinge motor rotate with degrees per second
+			// adapting this for rad/s
+			motor.targetVelocity = 3600;
 			motor.freeSpin = false;
 			hinge.motor = motor;
 			hinge.useMotor = true;
-			Debug.Log(hinge.velocity);
 		}
 		else{
 			hinge.useMotor = false;
 			motorIsOn = false;
 			AxisRigidbody.freezeRotation = true;
-			motorRenderer.material.SetColor("_Color",colorOff);
-			Debug.Log(hinge.velocity);
+			motorButtonRenderer.material.SetColor("_Color",colorOff);
 		}
 		
-
-        
-        
-        
+           
        
     }
 
--- Assets/Switch_action.cs
+++ Assets/Switch_action.cs
@@ -8,6 +8,7 @@ public class Switch_action : MonoBehaviour{
     public bool switchIsOn;
 	Color colorOn = Color.green;
 	Color colorOff = Color.red;
+
 	
     void OnMouseDown(){ //THIS FUNCTION WILL DETECT THE MOUSE CLICK ON A COLLIDER,IN OUR CASE WILL DETECT THE CLICK ON THE BUTTON
 		// Get the renderer from Switch
